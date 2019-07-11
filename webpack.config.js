458c4553e91a47472f7f6ee707d72fac525c9922
Jan Neuman <neuman.jan@gmail.com>
2019-07-11 18:09:39 +0200

    update dependencies and project config

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,58 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var MiniCssExtractPlugin = require("mini-css-extract-plugin");
-var webpack = require("webpack");
-var path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
-var basePath = __dirname;
+const basePath = __dirname;
 
 module.exports = {
-  context: path.join(basePath, "src"),
+  context: path.join(basePath, 'src'),
   resolve: {
-    extensions: [".js", ".ts", ".tsx"]
+    extensions: ['.js', '.ts', '.tsx'],
   },
-  entry: ["@babel/polyfill", "./index.tsx"],
+  entry: ['@babel/polyfill', './index.tsx'],
   output: {
-    path: path.join(basePath, "dist"),
-    filename: "bundle.js"
+    path: path.join(basePath, 'dist'),
+    filename: 'bundle.js',
   },
   externals: {
-    'react': 'React',
+    react: 'React',
     'react-dom': 'ReactDOM',
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   devServer: {
-    contentBase: "./dist", // Content base
+    contentBase: './dist', // Content base
     inline: true, // Enable watch and live reload
-    host: "localhost",
+    host: 'localhost',
     port: 8080,
-    stats: "errors-only"
+    stats: 'errors-only',
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        loader: "awesome-typescript-loader",
+        loader: 'awesome-typescript-loader',
         options: {
           useBabel: true,
-          babelCore: "@babel/core" // needed for Babel v7
-        }
+          babelCore: '@babel/core', // needed for Babel v7
+        },
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: "file-loader",
-        options: {
-          name: "assets/img/[name].[ext]?[hash]"
-        }
-      }
-    ]
+    ],
   },
   plugins: [
-    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+    // Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: "index.html", //Name of file in ./dist/
-      template: "index.html", //Name of template in ./src
-      hash: true
+      filename: 'index.html', // Name of file in ./dist/
+      template: 'index.html', // Name of template in ./src
+      hash: true,
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 };
